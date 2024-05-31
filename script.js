ab6a1afad45e0ec35f5a8be03a516253107b466b
SufiyanAK <Sufiyankhattak14@gmail.com>
2024-05-31 22:25:21 +0500

    added signUp Form and make it toggle between SignIn and SignUp

--- script.js
+++ script.js
@@ -1,13 +1,71 @@
 // SELECTORS
-const inputs = document.querySelectorAll('.input')
+const signInForm = document.querySelector('#signInForm')
+const signUpForm = document.querySelector('#signUpForm')
+const tabs = document.querySelectorAll('.heading');
+const signInInputs = document.querySelectorAll('.signIn-input');
+const signUpInputs = document.querySelectorAll('.signUp-input');
 const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirmPassword');
+const signInBtn = document.querySelector('#signInBtn');
 const signUpBtn = document.querySelector('#signUpBtn');
 
 // VARIABLE
 const data = []
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(otherTab => {
+            if (tab !== otherTab) {
+                otherTab.classList.remove('border-bottom');
+                const form = otherTab.dataset.id
+                if (form === 'signInForm') {
+                    signInForm.style.display = 'none'
+                    signUpForm.style.display = 'grid'
+                } else {
+                    signInForm.style.display = 'grid'
+                    signUpForm.style.display = 'none'
+
+                }
+            }
+        })
+        tab.classList.add('border-bottom');
+    })
+})
+
+// EVENTLISTNER TO PUSH DATA INTO ARRAY OF OBJECT
+signUpBtn.addEventListener('click', () => {
+    const value = getData(name, email, password, confirmPassword)
+    if (value.password === value.confirmPassword) {
+        data.push(value);
+    }
+})
+
+// EVENT LISTENERS FOR SIGN IN INPUT FIELDS
+signInInputs.forEach(input => {
+    input.addEventListener('input', checkInput(signInInputs, signInBtn))
+});
+
+// EVENT LISTENERS FOR SIGN UP INPUT FIELDS
+signUpInputs.forEach(input => {
+    input.addEventListener('input', checkInput(signUpInputs, signUpBtn))
+});
+
+function checkInput(input, btn) {
+    let allFilled = true;
+    input.forEach(input => {
+        if (!input.value.trim()) {
+            allFilled = false;
+            btn.classList.remove('enabled')
+        }
+    });
+    btn.disabled = !allFilled;
+    if (!btn.disabled) {
+        btn.classList.add('enabled')
+    }
+    console.log(btn.disabled);
+}
 
 // FUNCTION TO GET DATA FROM INPUT FIELDS
 function getData(name, email, password, confirmPassword) {
@@ -17,42 +75,10 @@ function getData(name, email, password, confirmPassword) {
         password: password.value,
         confirmPassword: confirmPassword.value
     }
+
+    name.value = ''
+    email.value = ''
+    password.value = ''
+    confirmPassword.value = ''
     return values
 }
-
-// EVENTLISTNER TO PUSH DATA INTO ARRAY OF OBJECT
-signUpBtn.addEventListener('click', () => {
-    const value = getData(name, email, password, confirmPassword)
-    console.log(value);
-    if (value.password === value.confirmPassword) {
-        data.push(value);
-        console.log(data);
-    }
-
-    console.log(password);
-    console.log(confirmPassword);
-
-})
-
-function checkInput() {
-    let allFilled = true;
-    inputs.forEach(input => {
-        if (!input.value.trim()) {
-            allFilled = false;
-            signUpBtn.classList.remove('enabled')
-        }
-    });
-    signUpBtn.disabled = !allFilled;
-    if (!signUpBtn.disabled) {
-        signUpBtn.classList.add('enabled')
-    }
-    console.log(signUpBtn.disabled);
-}
-
-// EVENT LISTENERS FOR INPUT FIELDS
-inputs.forEach(input => {
-    input.addEventListener('input', checkInput)
-});
-
-// INITIAL CHECK TO DISABLE BUTTON IF FIELDS ARE EMPTY
-checkInput();
