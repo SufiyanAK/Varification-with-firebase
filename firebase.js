e8fe014a254df59dd04d0bee7347f9f60bf6bd88
SufiyanAK <Sufiyankhattak14@gmail.com>
2024-05-31 15:44:59 +0500

    set the basic workflow for firebase app

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWs90Ym_Gg3PoUcaeabydQ0q676htgBmM",
+  authDomain: "fir-learn-f849e.firebaseapp.com",
+  projectId: "fir-learn-f849e",
+  storageBucket: "fir-learn-f849e.appspot.com",
+  messagingSenderId: "1088878641220",
+  appId: "1:1088878641220:web:553fed2445e668e8d03c11"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
