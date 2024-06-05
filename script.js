37641c26314f16b43dfe22fcbdb216d39073f35e
SufiyanAK <Sufiyankhattak14@gmail.com>
2024-06-05 09:00:27 +0500

    removed unnecessary code and added comments to define each code block

--- script.js
+++ script.js
@@ -10,65 +10,60 @@ const signUpBtn = document.querySelector('#signUpBtn');
 const data = []
 const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
+// SIGNUP INPUTS EVENTLISTNER TO CHECK FOR EMPTY INPUT
 signUpInputs.forEach(input => {
     input.addEventListener('input', () => checkInput(signUpInputs, signUpBtn))
 })
 
+// SIGNUP BTN EVENTLISTNER
 signUpBtn.addEventListener('click', () => {
     const getValues = getData(name, signUpEmail, signUpPassword, confirmPassword);
 
     const valid = checkValidation(getValues);
-    console.log(valid);
 
     if (valid) {
         data.push(getValues);
+
+        name.value = '';
+        signUpEmail.value = '';
+        signUpPassword.value = '';
+        confirmPassword.value = '';
     }
 
 })
 
-
-
 // FUNCTION TO GET DATA FROM SIGN UP INPUTS
 function getData(n, se, sp, cp) {
-    const value = {
+    return {
         name: n.value,
         email: se.value,
         password: sp.value,
         confirmPassword: cp.value
     }
-
-    n.value = ''
-    se.value = ''
-    sp.value = ''
-    cp.value = ''
-
-    return value
 }
 
 // FUNCTION TO CHECK EMPTY SIGN UP INPUTS
 function checkInput(input, btn) {
     let allFilled = true;
+
     input.forEach(input => {
         if (!input.value.trim()) {
             allFilled = false;
             btn.classList.add('not-filled');
         }
     });
+
     btn.disabled = !allFilled;
+
     if (!btn.disabled) {
         btn.classList.remove('not-filled')
     }
-    console.log(btn.disabled);
 }
 
 // FUNCTION TO CHECK EMAIL VALIDATION AND PASSWORD CONFIRMATION
 function checkValidation(data) {
 
     const { email, password, confirmPassword } = data;
-    console.log(email);
-    console.log(password);
-    console.log(confirmPassword);
-    console.log(emailRegEx.test(email));
 
     if (!emailRegEx.test(email) || password !== confirmPassword) {
         return false;
